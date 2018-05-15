97fe87680760031ce147f2791bc86e565fffa7a9
ahireshashank <ahireshashank91@gmail.com>
2018-05-15 12:43:11 +0530

    File uploads

---
+++ FindIfAFileExists.py
@@ -0,0 +1,25 @@
+
+
+import os
+
+# Path IN which we have to search file
+PATH = '/home/shashank/Documents/GITs/Python-Programs/Scripts'   # Give your path here
+
+def searchFile(fileName):
+    ''' This function searches for the specified file name in the given PATH '''
+    for root, dirs, files in os.walk(PATH):
+        print('Looking in:',root)
+        for Files in files:
+            try:
+                found = Files.find(fileName)        # Returns -1 if NOT found else returns index
+                # print(found)
+                if found != -1:
+                    print(fileName, 'Found')
+                    break
+            except:
+                exit()
+
+if __name__ == '__main__':
+    searchFile('6-Folder.txt')
+
+  
